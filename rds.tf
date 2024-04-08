e418bf9240743ff63028d2d3a13e1dc17ae1ade7
Ibeawuchi411 <142343304+Ibeawuchi411@users.noreply.github.com>
2024-04-08 12:47:15 +0100

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -25,7 +25,7 @@ resource "aws_db_instance" "database_instance" {
   skip_final_snapshot     = true
   availability_zone       = "us-west-1b"
   identifier              = var.database_instance_identifier
-  snapshot_identifier     = data.aws_db_snapshot.latest_db_snapshot.id
+  snapshot_identifier     = arn:aws:rds:us-east-2:736741838258:snapshot:myrds-snapshot  
   db_subnet_group_name    = aws_db_subnet_group.database_subnet_group.name
   multi_az                = var.multi_az_deployment
   vpc_security_group_ids  = [aws_security_group.database_security_group.id]
